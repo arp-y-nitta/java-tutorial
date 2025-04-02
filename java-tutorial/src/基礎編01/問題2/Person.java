281d3b9963afb42443bce2ead3875b1759ad09b6
arp-y-nitta <y.nitta.arp@gmail.com>
2025-04-02 19:54:26 +0900

    mainメソッドを修正

--- java-tutorial/src/基礎編01/問題2/Person.java
+++ java-tutorial/src/基礎編01/問題2/Person.java
@@ -12,7 +12,7 @@ public class Person {
 		System.out.println("名前:" + name);
 		System.out.println("年齢:" + age);
 	}
-	public void main(String[] args) {
+	public static void main(String[] args) {
 		if (args.length != 2) {
 			System.out.println("年齢と名前を指定してください");
 			return;
