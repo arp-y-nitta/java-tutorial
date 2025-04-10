d4e7823a15d06b3e9514dc1019ffba7634e0e65c
arp-y-nitta <y.nitta.arp@gmail.com>
2025-04-10 12:26:59 +0900

    基礎編03完了

---
+++ java-tutorial/src/基礎編03/写経問題/Main3.java
@@ -0,0 +1,11 @@
+package 基礎編03.写経問題;
+
+public class Main3 {
+
+	public static void main(String[] args) {
+		int[] numbers = {1, 2, 3, 4, 5};
+		for (int i = 0; i < numbers.length; i++) {
+			System.out.println(numbers[i]);
+		}
+	}
+}
---
+++ java-tutorial/src/基礎編03/確認問題/Result.java
@@ -0,0 +1,16 @@
+package 基礎編03.確認問題;
+//条件分岐問題
+public class Result {
+
+	public static void main(String[] args) {
+		int score = 78;
+		if (score >= 90) {
+			System.out.println("優秀です！");
+		} else if (score >= 70) {
+			System.out.println("合格です！");
+		} else {
+			System.out.println("不合格です...");
+		} 
+	}
+
+}
